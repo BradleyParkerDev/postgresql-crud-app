7d5aa09ea5fb430bab139aa4db865eec15ffe415
BradleyParkerDev <bradleyscottparker@outlook.com>
2024-09-22 08:45:00 -0500

    updated example.env

---
+++ src/database/migrations/0000_cultured_mother_askani.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"user_image" text,
+	"first_name" text NOT NULL,
+	"last_name" text NOT NULL,
+	"email_address" text NOT NULL,
+	"user_name" text NOT NULL,
+	"password" text NOT NULL,
+	"last_updated" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_id_unique" UNIQUE("id"),
+	CONSTRAINT "users_email_address_unique" UNIQUE("email_address"),
+	CONSTRAINT "users_user_name_unique" UNIQUE("user_name")
+);
